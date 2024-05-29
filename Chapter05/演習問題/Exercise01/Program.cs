06972e21266048f6ea8721ba20c22caa58c5cd37
FujiiShizen <oibc35113@ojs.ac.jp>
2024-05-29 15:50:00 +0900

    第5章演習問題5.5.1終了

---
+++ Chapter05/演習問題/Exercise01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("文字1を入力：");
+            string str1 = Console.ReadLine();
+            Console.Write("文字2を入力：");
+            string str2 = Console.ReadLine();
+
+            if (String.Compare(str1, str2, ignoreCase:true) == 0) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+        }
+    }
+}
---
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("数字を入力：");
+            string str1 = Console.ReadLine();
+            int.TryParse(str1, out int num1);
+            var num2 = num1.ToString("#,0");
+            
+        }
+    }
+}
