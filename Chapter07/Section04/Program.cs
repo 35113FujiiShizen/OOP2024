91bb2c4af4c246448925c8a83b090f83fd92cf7f
FujiiShizen <oibc35113@ojs.ac.jp>
2024-06-19 15:01:34 +0900

    7.2.2完成

--- Chapter07/Section04/Program.cs
+++ Chapter07/Section04/Program.cs
@@ -14,6 +14,11 @@ namespace Section04 {
             abbrs.Add("IOC", "国際オリンピック委員会");
             abbrs.Add("NPT", "核兵器不拡散条約");
 
+
+
+
+
+
             // インデクサの利用例
             var names = new[] { "WHO", "FIFA", "NPT", };
             foreach (var name in names) {
