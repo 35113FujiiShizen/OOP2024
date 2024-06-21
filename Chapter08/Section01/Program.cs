b188fc226c5a4c14dea818982a9da5b4b215d58a
FujiiShizen <oibc35113@ojs.ac.jp>
2024-06-21 11:23:53 +0900

    誕生日と生まれてから何日目か表示する完成

--- Chapter08/Section01/Program.cs
+++ Chapter08/Section01/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,23 +8,23 @@ using System.Threading.Tasks;
 namespace Section01 {
     internal class Program {
         static void Main(string[] args) {
-            var dt1 = new DateTime(2024, 6, 19);
-            var dt2 = new DateTime(2005, 1, 4, 8, 45, 20);
-            Console.WriteLine(dt1);
-            Console.WriteLine(dt2);
+            //var dt1 = new DateTime(2024, 6, 19);
+            //var dt2 = new DateTime(2005, 1, 4, 8, 45, 20);
+            //Console.WriteLine(dt1);
+            //Console.WriteLine(dt2);
 
-            var today = DateTime.Today;
-            var now = DateTime.Now;
-            Console.WriteLine("Today : {0}", today);
-            Console.WriteLine("Now : {0}", now);
+            //var today = DateTime.Today;
+            //var now = DateTime.Now;
+            //Console.WriteLine("Today : {0}", today);
+            //Console.WriteLine("Now : {0}", now);
 
-            var isLeapYear = DateTime.IsLeapYear(2023);
-            if (isLeapYear) {
-                Console.WriteLine("閏年です");
-            } else {
-                Console.WriteLine("閏年ではありません");
-            }
-            Console.WriteLine("-----------------------");
+            //var isLeapYear = DateTime.IsLeapYear(2023);
+            //if (isLeapYear) {
+            //    Console.WriteLine("閏年です");
+            //} else {
+            //    Console.WriteLine("閏年ではありません");
+            //}
+            //Console.WriteLine("-----------------------");
 
             Console.WriteLine("生年月日を入力");
             Console.Write("年：");
@@ -35,10 +36,20 @@ namespace Section01 {
 
             var birthday = new DateTime(year, month, day);
 
-            DayOfWeek dayOfWeek = birthday.DayOfWeek;
-            string[] japaneseDays = { "日", "月", "火", "水", "木", "金", "土" };
-            string japaneseDayOfWeek = japaneseDays[(int)dayOfWeek];
-            Console.WriteLine("あなたは" + japaneseDayOfWeek + "曜日に生まれました");
+            //何曜日にうまれたか表示するプログラム↓
+            //DayOfWeek dayOfWeek = birthday.DayOfWeek;
+            //string[] japaneseDays = { "日", "月", "火", "水", "木", "金", "土" };
+            //string japaneseDayOfWeek = japaneseDays[(int)dayOfWeek];
+            //Console.WriteLine("あなたは" + japaneseDayOfWeek + "曜日に生まれました");
+
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            var str = birthday.ToString("ggyy年M月d日dddd", culture);
+            Console.WriteLine("あなたは"+str+"に生まれました");
+
+            var nowDay = DateTime.Now;
+            TimeSpan diff = nowDay.Date - birthday.Date;
+            Console.WriteLine("あなたは生まれてから今日で{0}日目です", diff.Days);
         }
     }
 }
