0531c7a5bff1d4a349c7ea61cfc43ad37658a293
FujiiShizen <oibc35113@ojs.ac.jp>
2024-09-06 12:09:00 +0900

    9月6日終了

--- FormApps/RssReader/Form1.Designer.cs
+++ FormApps/RssReader/Form1.Designer.cs
@@ -23,23 +23,18 @@
         /// コード エディターで変更しないでください。
         /// </summary>
         private void InitializeComponent() {
-            this.tbRssUrl = new System.Windows.Forms.TextBox();
             this.tbGet = new System.Windows.Forms.Button();
             this.lbRssTitle = new System.Windows.Forms.ListBox();
-            this.webView1 = new Microsoft.Toolkit.Forms.UI.Controls.WebView();
-            ((System.ComponentModel.ISupportInitialize)(this.webView1)).BeginInit();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
             this.SuspendLayout();
-            // 
-            // tbRssUrl
-            // 
-            this.tbRssUrl.Location = new System.Drawing.Point(29, 21);
-            this.tbRssUrl.Name = "tbRssUrl";
-            this.tbRssUrl.Size = new System.Drawing.Size(582, 19);
-            this.tbRssUrl.TabIndex = 0;
             // 
             // tbGet
             // 
-            this.tbGet.Location = new System.Drawing.Point(617, 21);
+            this.tbGet.Location = new System.Drawing.Point(652, 24);
             this.tbGet.Name = "tbGet";
             this.tbGet.Size = new System.Drawing.Size(85, 20);
             this.tbGet.TabIndex = 1;
@@ -51,43 +46,75 @@
             // 
             this.lbRssTitle.FormattingEnabled = true;
             this.lbRssTitle.ItemHeight = 12;
-            this.lbRssTitle.Location = new System.Drawing.Point(29, 72);
+            this.lbRssTitle.Location = new System.Drawing.Point(29, 132);
             this.lbRssTitle.Name = "lbRssTitle";
-            this.lbRssTitle.Size = new System.Drawing.Size(271, 532);
+            this.lbRssTitle.Size = new System.Drawing.Size(271, 472);
             this.lbRssTitle.TabIndex = 2;
             this.lbRssTitle.SelectedIndexChanged += new System.EventHandler(this.lbRssTitle_SelectedIndexChanged);
             // 
-            // webView1
+            // webView21
             // 
-            this.webView1.Location = new System.Drawing.Point(306, 72);
-            this.webView1.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webView1.Name = "webView1";
-            this.webView1.Size = new System.Drawing.Size(543, 506);
-            this.webView1.TabIndex = 3;
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(306, 132);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(582, 472);
+            this.webView21.TabIndex = 3;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(146, 24);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(500, 20);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Yahooニューストピックス\r\nまたはRSS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "【お気に入り登録】\r\nRSSを入力した状態で登録すること。\r\n";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(917, 631);
-            this.Controls.Add(this.webView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.webView21);
             this.Controls.Add(this.lbRssTitle);
             this.Controls.Add(this.tbGet);
-            this.Controls.Add(this.tbRssUrl);
             this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.webView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox tbRssUrl;
         private System.Windows.Forms.Button tbGet;
         private System.Windows.Forms.ListBox lbRssTitle;
-        private Microsoft.Toolkit.Forms.UI.Controls.WebView webView1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- FormApps/RssReader/Form1.cs
+++ FormApps/RssReader/Form1.cs
@@ -1,26 +1,43 @@
-﻿using System;
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Net;
+using System.Security.Policy;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace RssReader {
-    //データ格納用クラス
-    public class RssItem {
-        public string Title { get; set; }
-        public string Link { get; set; }
-    }
+
     public partial class Form1 : Form {
         List<RssItem> xitems;
         public Form1() {
             InitializeComponent();
+            var rssDict = new Dictionary<string, string>() {
+                {"https://news.yahoo.co.jp/rss/topics/top-picks.xml","主要"},
+                {"https://news.yahoo.co.jp/rss/topics/domestic.xml","国内"},
+                {"https://news.yahoo.co.jp/rss/topics/world.xml","国際"},
+                {"https://news.yahoo.co.jp/rss/topics/business.xml","経済"},
+                {"https://news.yahoo.co.jp/rss/topics/entertainment.xml","エンタメ" },
+                {"https://news.yahoo.co.jp/rss/topics/sports.xml","スポーツ"},
+                {"https://news.yahoo.co.jp/rss/topics/it.xml","IT"},
+                {"https://news.yahoo.co.jp/rss/topics/science.xml","科学"},
+                {"https://news.yahoo.co.jp/rss/topics/local.xml","地域"},
+            };
+            comboBox1.Items.AddRange(rssDict.Keys.ToArray());
+            comboBox1.DataSource = new BindingSource(rssDict, null);
+            comboBox1.DisplayMember = "Value";
+            comboBox1.ValueMember = "Key";
         }
+
 
         private void tbGet_Click(object sender, EventArgs e) {
             using (var wc = new WebClient()) {
-                var url = wc.OpenRead(tbRssUrl.Text);
+                var url = wc.OpenRead(comboBox1.Text);
                 var xdoc = XDocument.Load(url);
                 //var xitems = xdoc.Root.Descendants("item");
                 xitems = xdoc.Root.Descendants("item")
@@ -36,8 +53,23 @@ namespace RssReader {
         }
 
         private void lbRssTitle_SelectedIndexChanged(object sender, EventArgs e) {
-            webView1.Navigate(xitems[lbRssTitle.SelectedIndex].Link);
+            if (lbRssTitle.SelectedItem != null) {
+                var selectedTitle = lbRssTitle.SelectedItem.ToString();
+                var selectedItem = xitems.FirstOrDefault(item => item.Title == selectedTitle);
+
+                if (selectedItem != null) {
+                    webView21.Source = new Uri(selectedItem.Link);
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) {
+            
         }
     }
-
+    //データ格納用クラス
+    public class RssItem {
+        public string Title { get; set; }
+        public string Link { get; set; }
+    }
 }
