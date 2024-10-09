b0510bc9cd8b05c5c0b438eb43260f6e4a92d5af
FujiiShizen <oibc35113@ojs.ac.jp>
2024-10-09 14:57:39 +0900

    TemplateMethodパターン完成

--- Chapter17/TemplateMethod/LineCounter/Program.cs
+++ Chapter17/TemplateMethod/LineCounter/Program.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextFileProcessor;
 
-namespace TextFileProcessor {
+namespace LineCounter {
     internal class Program {
         static void Main(string[] args) {
+            TextProcessor.Run<LineCounterProcessor>(args[0]);
         }
     }
 }
