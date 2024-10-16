02d9c05f2b93121c6fdd9f5916604115eeeaede7
FujiiShizen <oibc35113@ojs.ac.jp>
2024-10-16 15:07:46 +0900

    演習17-3まで完成！

--- Chapter17/TemplateMethod/TextNumberSizeChange/ToHankakuProcessor.cs
+++ Chapter17/TemplateMethod/TextNumberSizeChange/ToHankakuProcessor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using TextNumberSizeChange.Framework;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,31 +7,32 @@ using System.Text;
 using System.Threading.Tasks;
 using TextFileProcessor;
 using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
 
 namespace TextNumberSizeChange {
-    class ToHankakuProcessor:TextProcessor {
+    class ToHankakuProcessor:ITextFileService {
         Dictionary<char,char> conv = new Dictionary<char, char>() {
         {'１','1'},{'２','2'},{'３','3'},{'４','4'},{'５','5'},
         {'６','6'},{'７','7'},{'８','8'},{'９','9'},{'０','0'}
         };
         private int _count;
         string _text ="";
-        
 
-        protected override void Initialize(string fname) {
+        public void Initialize(string fname) {
             _count = 0;
         }
-
-        protected override void Execute(string line) {
+        public void Execute(string line) {
             string numberLower = new string
-                (line.Select(n => (conv.ContainsKey(n) ? conv[n] : n)).ToArray());
+                    (line.Select(n => (conv.ContainsKey(n) ? conv[n] : n)).ToArray());
             Console.WriteLine(numberLower);
             _count++;
         }
 
-        protected override void Terminate() {
-            Console.WriteLine("{0}行",_count);
-            Console.WriteLine(_text);
+
+
+        public void Terminate() {
+                Console.WriteLine("{0}行",_count);
+                Console.WriteLine(_text);
         }
     }
 }
