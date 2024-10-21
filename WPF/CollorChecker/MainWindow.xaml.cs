46d0ccdf6c225bd48230092b611f828981dea4a6
FujiiShizen <oibc35113@ojs.ac.jp>
2024-10-21 16:00:16 +0900

    10/21シュウリョウ！

--- WPF/CollorChecker/MainWindow.xaml.cs
+++ WPF/CollorChecker/MainWindow.xaml.cs
@@ -18,8 +18,23 @@ namespace CollorChecker {
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
+        private byte alpha = 255; // alphaをフィールドとして定義
+
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            var rvalue = (int)rSlider.Value;
+            rValue.Text = rvalue.ToString();
+            var gvalue = (int)gSlider.Value;
+            gValue.Text = gvalue.ToString();
+            var bvalue = (int)bSlider.Value;
+            bValue.Text = bvalue.ToString();
+            byte alpha = 255;
+
+            
+            colorArea.Background = new SolidColorBrush(Color.FromArgb(alpha, (byte)rvalue, (byte)gvalue, (byte)bvalue));
         }
     }
 }
